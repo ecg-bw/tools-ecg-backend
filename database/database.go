34b6a96427ca28ad84407e3b3bbaafb9b059e642
jonasschmalzhaf <jonasschmalzhaf@gmail.com>
2026-09-23 12:12:53 +0200

    fix: change migration path

--- database/database.go
+++ database/database.go
@@ -10,7 +10,7 @@ import (
 	"github.com/golang-migrate/migrate/v4/source/iofs"
 )
 
-//go:embed ../../migrations/*.sql
+//go:embed migrations/*.sql
 var migrationFiles embed.FS
 
 func RunMigrations(db *sql.DB) error {
